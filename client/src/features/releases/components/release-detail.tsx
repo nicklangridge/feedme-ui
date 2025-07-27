5f95a2223f69611d0a9ff2df6abbe104feb953a5
Nick Langridge <nicklangridge@hotmail.com>
2025-07-27 14:31:57 +0100

    Split release details into separate componets
    Later can split them to separate files maybe

--- client/src/features/releases/components/release-detail.tsx
+++ client/src/features/releases/components/release-detail.tsx
@@ -17,6 +17,11 @@ import { Link as RouterLink, useNavigate } from 'react-router';
 import Spinner from '@/components/ui/spinner';
 import { useRelease } from '../api/get-release';
 import { paths } from '@/config/paths';
+import { type Release } from '@/types/api';
+
+type ReleaseDetailProps = {
+  releaseId: number;
+};
 
 function Loading() {
   return (
@@ -32,10 +37,6 @@ function NotFound() {
   );
 }
  
-type ReleaseDetailProps = {
-  releaseId: number;
-};
-
 function navigateBack(navigate: ReturnType<typeof useNavigate>) {
   if (document.referrer != "") {
     navigate(-1);
@@ -63,86 +64,103 @@ export default function ReleaseDetail({ releaseId }: ReleaseDetailProps) {
       </Box>
       <Grid container spacing={2}>
         <Grid size={{xs: 12, sm: 5, md: 5, lg: 4, xl: 6 }}>
-          <Card>
-            <Box
-            component="img"
-            sx={{ width: '100%', cursor: 'pointer' }}
-            title={ release.album_name }
-            src={ release.image }
-            onClick={() => { window.location.href = release.album_uri }}
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h3" component="div" sx={{ mb: 0.2 }}>
-                { release.album_name }
-              </Typography>
-              <Typography gutterBottom variant="h4" component="div" sx={{ mb: 0 }}>
-                by { release.artist_name }
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button startIcon={<PlayCircleIcon />} color="inherit" onClick={() => { window.location.href = release.album_uri }}>Spotify</Button>
-              <Button startIcon={<ShareIcon />} color="inherit" onClick={()=>navigator.share({title: release.album_name, url: location.href })}>Share</Button>
-            </CardActions>
-          </Card>
+          <ReleaseInfo release={release} />
         </Grid>
         <Grid size={{xs: 12, sm: 7, md: 7, lg: 8, xl: 6 }}>
-          <Card>
-            <CardContent>
-              <Typography variant="h5">
-                Reviews
-              </Typography>
-              { release.reviews.map((review) => (
-                <>
-                  <Typography variant="h6" sx={{ mt: 2 }}>
-                    { review.name }
-                  </Typography>
-                  <Typography variant="body2" component="div" sx={{ color: 'text.secondary', mt: '0.6rem' }}>
-                    { review.snippet || 'No preview available' }
-                  </Typography>
-                  <Button href={ review.url } endIcon={<OpenInNewIcon />} target="_blank" rel="noopener noreferrer" color="inherit" variant="contained" size="small" sx={{ mt: 1 }}>
-                    Read more
-                  </Button>
-                </>
-              ))}
-            </CardContent>
-          </Card>
-          { (release.genres.length > 0 || release.reviews.length > 0) && (
-            <Card sx={{ mt: 2 }}>
-              <CardContent>
-                <Typography variant="h5" sx={{ mb: 2 }}>
-                  Tags
-                </Typography>
-                <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 1,  p: 0 }}>
-                  { release.reviews.map((review) => (
-                    <Chip
-                    key={review.name}
-                    label={review.name}
-                    color="secondary"
-                    component={ RouterLink }
-                    clickable
-                    to={ paths.feed.getHref(review.slug) }
-                    icon={<RssFeedIcon />}
-                    />
-                  ))}
-                </Box>
-                <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 1, mt: 2, mb: 0, p: 0 }}>
-                  { release.genres.map((genre) => (
-                    <Chip 
-                    key={genre.name}
-                      label={genre.name}
-                      color="secondary"
-                      component={ RouterLink }
-                      clickable
-                      to={ paths.genre.getHref(genre.slug) }
-                      icon={<SellIcon />}
-                      />
-                  ))}
-                </Box>
-              </CardContent>
-            </Card>
-          )}
+          <ReleaseReviews release={release} />
+          <ReleaseTags release={release} />
         </Grid>
       </Grid>
     </>
   );
 };
+
+function ReleaseInfo({ release }: { release: Release }) {
+  return (
+    <Card>
+      <Box
+      component="img"
+      sx={{ width: '100%', cursor: 'pointer' }}
+      title={ release.album_name }
+      src={ release.image }
+      onClick={() => { window.location.href = release.album_uri }}
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h3" component="div" sx={{ mb: 0.2 }}>
+          { release.album_name }
+        </Typography>
+        <Typography gutterBottom variant="h4" component="div" sx={{ mb: 0 }}>
+          by { release.artist_name }
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button startIcon={<PlayCircleIcon />} color="inherit" onClick={() => { window.location.href = release.album_uri }}>Spotify</Button>
+        <Button startIcon={<ShareIcon />} color="inherit" onClick={()=>navigator.share({title: release.album_name, url: location.href })}>Share</Button>
+      </CardActions>
+    </Card>
+  );
+}
+
+function ReleaseReviews({ release }: { release: Release }) {
+  if (release.reviews.length <= 0) return null;
+  return (
+    <Card>
+      <CardContent>
+        <Typography variant="h5"> 
+          Reviews
+        </Typography>
+        { release.reviews.map((review) => (
+          <>
+            <Typography variant="h6" sx={{ mt: 2 }}>
+              { review.name }
+            </Typography>
+            <Typography variant="body2" component="div" sx={{ color: 'text.secondary', mt: '0.6rem' }}>
+              { review.snippet || 'No preview available' }
+            </Typography>
+            <Button href={ review.url } endIcon={<OpenInNewIcon />} target="_blank" rel="noopener noreferrer" color="inherit" variant="contained" size="small" sx={{ mt: 1 }}>
+              Read more
+            </Button>
+          </>
+        ))}
+      </CardContent>
+    </Card>
+  );
+}
+
+function ReleaseTags({ release }: { release: Release }) {
+  if (release.genres.length <= 0 || release.reviews.length <= 0) return null;
+  return (
+    <Card sx={{ mt: 2 }}>
+      <CardContent>
+        <Typography variant="h5" sx={{ mb: 2 }}>
+          Tags
+        </Typography>
+        <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 1,  p: 0 }}>
+          { release.reviews.map((review) => (
+            <ReleaseTag label={review.name} type="feed" to={paths.feed.getHref(review.slug)} />
+          ))}
+        </Box>
+        <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 1, mt: 2, mb: 0, p: 0 }}>
+          { release.genres.map((genre) => (
+            <ReleaseTag label={genre.name} type="genre" to={paths.genre.getHref(genre.slug)} />
+          ))}
+        </Box>
+      </CardContent>
+    </Card>
+  )
+}
+
+function ReleaseTag({ label, type, to }: { label: string; type: "feed" | "genre"; to: string }) {
+  const icon = type === "feed" ? <RssFeedIcon /> : <SellIcon />;
+  return (
+    <Chip
+      key={label}
+      label={label}
+      color="secondary"
+      component={RouterLink}
+      clickable
+      to={to}
+      icon={icon}
+    />
+  );
+}
