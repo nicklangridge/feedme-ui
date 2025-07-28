e5807b25d56e8a186d7094c62342532418b1595d
Nick Langridge <nicklangridge@hotmail.com>
2025-07-28 14:09:57 +0100

    More layout tweaks

--- client/src/features/releases/components/release-card.tsx
+++ client/src/features/releases/components/release-card.tsx
@@ -22,41 +22,43 @@ export default function ReleaseCard(release: Release) {
         to={ paths.release.getHref(release.album_id, release.album_slug) }
         sx={{ textDecoration: 'none', color: 'inherit', height: '100%' }}
       >
-        <CardContent sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
-        <CardMedia
-          sx={{ width: '100%', aspectRatio: '1 / 1', objectFit: 'cover' }}
-          image={ release.image}
-          title={ release.album_name }
-        />
-          <Typography gutterBottom variant="h5" component="div" sx={{ mb: 0.2 }}>
-            { release.album_name }
-          </Typography>
-          <Typography gutterBottom variant="h6" component="div" sx={{ mb: 0 }}>
-            by { release.artist_name }
-          </Typography>
-          { release.genres.length > 0 && (
-            <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 1, mt: 2, mb: 0, p: 0 }}>
-              { release.genres.slice(0, 3).map((genre) => (
-                <Chip key={genre.name} label={genre.name} size="small" color="secondary" icon={<SellIcon />} />
-              ))}
-            </Box>
-          )}
-          { release.reviews.length > 0 && (
-            <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 1, mt: 2, mb: 0, p: 0 }}>
-              { release.reviews.map((review) => (
-                <Chip key={review.name} label={review.name} size="small" color="secondary" icon={<RssFeedIcon />} />
-              ))}
-            </Box>
-          )}
-          { release.created && (
-            <>
-              <Box sx={{ flexGrow: 1 }}></Box>
-              <Typography variant="body2" sx={{ color: 'text.secondary', mt: 2, mb:0, fontSize : '0.8em' }}>
-                Found <TimeAgo date={release.created} />
-              </Typography>
-            </>
-          )}
-        </CardContent>
+        <Box sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
+          <CardMedia
+            sx={{ width: '100%', aspectRatio: '1 / 1', objectFit: 'cover' }}
+            image={ release.image}
+            title={ release.album_name }
+          />
+          <CardContent sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
+            <Typography gutterBottom variant="h5" component="div" sx={{ mb: 0.2 }}>
+              { release.album_name }
+            </Typography>
+            <Typography gutterBottom variant="h6" component="div" sx={{ mb: 0 }}>
+              by { release.artist_name }
+            </Typography>
+            { release.genres.length > 0 && (
+              <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 1, mt: 2, mb: 0, p: 0 }}>
+                { release.genres.slice(0, 3).map((genre) => (
+                  <Chip key={genre.name} label={genre.name} size="small" color="secondary" icon={<SellIcon />} />
+                ))}
+              </Box>
+            )}
+            { release.reviews.length > 0 && (
+              <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 1, mt: 2, mb: 0, p: 0 }}>
+                { release.reviews.map((review) => (
+                  <Chip key={review.name} label={review.name} size="small" color="secondary" icon={<RssFeedIcon />} />
+                ))}
+              </Box>
+            )}
+            { release.created && (
+              <>
+                <Box sx={{ flexGrow: 1 }}></Box>
+                <Typography variant="body2" sx={{ color: 'text.secondary', mt: 2, mb:0, fontSize : '0.8em' }}>
+                  Found <TimeAgo date={release.created} />
+                </Typography>
+              </>
+            )}
+          </CardContent>
+        </Box>
       </CardActionArea>
     </Card>
   );
