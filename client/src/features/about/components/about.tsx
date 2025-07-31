0cea13775fba264463518a1af37a905f2accef7a
Nick Langridge <nicklangridge@hotmail.com>
2025-07-31 14:38:16 +0100

    Add About content

--- client/src/features/about/components/about.tsx
+++ client/src/features/about/components/about.tsx
@@ -1,8 +1,25 @@
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+
 const About = () => {
   return (
-    <>
-      <p style={{ textAlign: 'center' }}>About page placeholder</p>
-    </>
+    <Card sx={{ width: 500, margin: '0 auto' }}>
+        <CardContent>
+          <Typography variant="h5"> 
+            Feed Me Music
+          </Typography> 
+          <p>
+            This website brings together album reviews from music sites all over the web and links them with albums in the Spotify catalogue.
+          </p>
+          <p>
+            The aim is to provide a simple way to get a daily fix of new music and discover new artists, albums and genres.
+          </p>
+          <p>
+            This is a non-commercial hobby project and is run on a best-effort basis. 
+          </p>
+      </CardContent>
+    </Card>
   );
 };
 
